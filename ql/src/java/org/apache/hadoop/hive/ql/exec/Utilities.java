ec1f40109cd7d64d26dbca9a1abc703fd307968f
Soam Acharya <soam@verticloud.com>
2013-10-09 10:55:43 -0700

    Quick hive patch as per AE-421

--- ql/src/java/org/apache/hadoop/hive/ql/exec/Utilities.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/Utilities.java
@@ -2413,8 +2413,5 @@ public final class Utilities {
 
     return sb.toString();
   }
-
-  public static Class getBuiltinUtilsClass() throws ClassNotFoundException {
-    return Class.forName("org.apache.hive.builtins.BuiltinUtils");
-  }
 }
+
